a02680e9f54c4b268752df63f4d53d131a7f6bc3
Thales Paiva <thalespaiva@gmail.com>
2016-03-02 22:47:51 -0300

    Esqueleto da classe de redes bayesianas

---
+++ bayesnets/src/bayesnets.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+class BayesNet:
+
+    def __init__(self):
+        pass
+
+    def init_from_bif_file(bif_file_path):
+        pass
+
+    def parse_bif_file(bif_file_path):
+        pass
