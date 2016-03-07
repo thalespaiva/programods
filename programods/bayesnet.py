03b9a20cb8aa637778f7ac3d36642a9cdceca786
Thales Paiva <thalespaiva@gmail.com>
2016-03-07 19:37:41 -0300

    Implementa resolução de consultas conjuntivas

--- programods/bayesnet.py
+++ programods/bayesnet.py
@@ -84,7 +84,7 @@ class BIF_Parser:
         main_var = variables[vars_info.pop(0)]
         cond_vars = [variables[v] for v in vars_info.pop(0)]
 
-        probability = Probability(main_var, cond_vars)
+        probability = Probability([main_var], cond_vars)
 
         probs_info = item.pop(0)
         if probs_info[0] == BIF_Parser.TABVALS_KEY:
@@ -99,27 +99,6 @@ class BIF_Parser:
                     probability.add_value(tuple(valuation), float(prob))
 
         return probability
-
-    # def get_data_from_file(file_path):
-    #     data_list = BIF_Parser.parse(file_path)
-
-    #     network_name = 'None'
-    #     variables = {}
-    #     probs = []
-    #     for item in data_list:
-    #         item_type = item.pop(0)
-    #         if item_type == BIF_Parser.NET_KEY:
-    #             network_name = item.pop(0)
-    #             network_properties = item.pop(0)
-
-    #         elif item_type == BIF_Parser.VAR_KEY:
-    #             variable = BIF_Parser._get_variable_from_data_item(item)
-    #             variables[variable.name] = variable
-
-    #         if item_type == BIF_Parser.PROB_KEY:
-    #             probs.append(BIF_Parser.
-    #                          _get_prob_from_data_item(item, variables))
-    #     return (network_name, variables, probs)
 
 
 class Function:
@@ -139,7 +118,7 @@ class Function:
         domains = [v.domain for v in self.variables]
 
         for valuation in it.product(*domains):
-            out.append('    %10s : %s\n' % (valuation, self.values[valuation]))
+            out.append('    %-25s : %10.5f\n' % (valuation, self.values[valuation]))
 
         return ''.join(out)
 
@@ -176,7 +155,7 @@ class Function:
 
         return division
 
-    def __mod__(self, variable):
+    def __sub__(self, variable):
         var_name = variable.name
 
         elim_vars = [v for v in self.variables if v.name != var_name]
@@ -196,10 +175,61 @@ class Function:
 
 class Probability(Function):
 
-    def __init__(self, main_var, cond_vars):
-        self.main_var = main_var
+    def __init__(self, main_vars, cond_vars):
+        self.main_vars = main_vars
         self.cond_vars = cond_vars
-        super().__init__([main_var] + cond_vars)
+        super().__init__(main_vars + cond_vars)
+
+    def __mod__(self, variable):
+        var_name = variable.name
+
+        main_elim_vars = [v for v in self.main_vars if v.name != var_name]
+        cond_elim_vars = [v for v in self.cond_vars if v.name != var_name]
+        elim_func = Probability(main_elim_vars, cond_elim_vars)
+
+        elim_var_names = [var.name for var in elim_func.variables]
+        total = 0
+        domains_list = [v.domain for v in elim_func.variables]
+        for elim_valuation in it.product(*domains_list):
+            var_valuation = dict(zip(elim_var_names, elim_valuation))
+            val_sum = 0
+            for value in variable.domain:
+                var_valuation[var_name] = value
+                val_sum += self.evaluate(var_valuation)
+            elim_func.add_value(elim_valuation, val_sum)
+            total += val_sum
+
+        for k, v in elim_func.values.items():
+            elim_func.values[k] = v/total
+        return elim_func
+
+    def __mul__(self, probab):
+        main_vars_set = set(self.main_vars) | set(probab.main_vars)
+        cond_vars_set = (set(self.cond_vars) | set(probab.cond_vars)) - \
+            main_vars_set
+        product = Probability(list(main_vars_set), list(cond_vars_set))
+
+        var_names = [var.name for var in product.variables]
+        for valuation in it.product(*[v.domain for v in product.variables]):
+            var_valuation = dict(zip(var_names, valuation))
+            val = self.evaluate(var_valuation)*probab.evaluate(var_valuation)
+            product.values[valuation] = val
+
+        return product
+
+    def __div__(self, probab):
+        main_vars_set = set(self.main_vars) | set(probab.main_vars)
+        cond_vars_set = (set(self.cond_vars) | set(probab.cond_vars)) - \
+            main_vars_set
+        division = Probability(list(main_vars_set), list(cond_vars_set))
+
+        var_names = [var.name for var in division.variables]
+        for valuation in it.product(*[v.domain for v in division.variables]):
+            var_valuation = dict(zip(var_names, valuation))
+            val = self.evaluate(var_valuation)*probab.evaluate(var_valuation)
+            division.values[valuation] = val
+
+        return division
 
 
 class Variable:
@@ -237,6 +267,8 @@ class BayesNet:
         self.probs = probs
         self.properties = properties
 
+        self.var_distribution = {}
+
     def parent_nodes(self, node_name):
         return [parent.name for parent in self.probs[node_name].cond_vars]
 
@@ -260,7 +292,7 @@ class BayesNet:
 
             if item_type == BIF_Parser.PROB_KEY:
                 prob = BIF_Parser._get_prob_from_data_item(item, nodes)
-                probs[prob.main_var.name] = prob
+                probs[prob.main_vars[0].name] = prob
 
         return BayesNet(network_name, nodes, probs, properties)
 
@@ -276,6 +308,52 @@ class BayesNet:
                 network.edge(parent, node)
         network.render(file_path, view=True)
 
-v = BayesNet.init_from_bif_file('../examples/bayesnet/asia/asia.bif')
-vs = v.nodes
-fs = v.probs
+    def query(valuation):  # valuation = {'name': 'value'}
+        # Factorization Theorem
+        pass
+
+    def get_var_distribution(self, var_name):
+        if not self.parent_nodes(var_name):
+            return self.probs[var_name]
+
+        prob = self.probs[var_name]
+        for parent in self.parent_nodes(var_name):
+            prob *= self.get_var_distribution(parent)
+        for var in prob.variables:
+            if var.name != var_name:
+                prob %= var
+
+        return prob
+
+    def get_ancestors_set(self, nodes):  # ancestor including the node
+        parent_nodes_set = set()
+        for node in nodes:
+            parent_nodes_set |= set(self.parent_nodes(node))
+        parent_nodes_not_in_nodes_set = parent_nodes_set - set(nodes)
+
+        if not parent_nodes_not_in_nodes_set:
+            return set()
+
+        return set(nodes) | parent_nodes_set | \
+            self.get_ancestors_set(parent_nodes_not_in_nodes_set)
+
+    def get_conjunctive(self, variables_names):
+        ancestors_set = self.get_ancestors_set(variables_names)
+
+        prob = self.probs[ancestors_set.pop()]
+        for var_name in ancestors_set:
+            prob *= self.probs[var_name]
+
+        variables_names_set = set(variables_names)
+        for var in prob.variables:
+            if var.name not in variables_names_set:
+                prob %= var
+
+        return prob
+
+
+if __name__ == "__main__":
+    v = BayesNet.init_from_bif_file('../examples/bayesnet/asia/asia.bif')
+    vs = v.nodes
+    fs = v.probs
+    n = BayesNet.init_from_bif_file('../examples/bayesnet/asia/rain.bif')
