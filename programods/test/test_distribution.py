a1e7144cef37fb691ad00278f504622faef97195
Thales Paiva <thalespaiva@gmail.com>
2016-03-26 19:43:52 -0300

    Adiciona testes para Variable.get_consistent_valuation()

--- programods/test/test_distribution.py
+++ programods/test/test_distribution.py
@@ -4,13 +4,19 @@ from ..distribution import Variable
 
 class TestVariable:
 
-    def test_get_valuation_X_1_Y_3(self):
+    def test_get_consistent_valuation_X_1_Y_3(self):
         X = Variable('X', range(2))
         Y = Variable('Y', range(4))
 
-        valuation = Variable.get_valuation([X, Y], [1, 3])
-        print(valuation)
+        valuation = Variable.get_consistent_valuation([X, Y], [1, 3])
         assert valuation == {'X': 1, 'Y': 3}
+
+    def test_get_consistent_valuation_for_inconsistent_valuation(self):
+        X = Variable('X', range(2))
+        Y = Variable('Y', range(4))
+
+        valuation = Variable.get_consistent_valuation([X, Y, X], [1, 3, 0])
+        assert valuation is None
 
     def test_cardinality(self):
         X = Variable('X', 8)
